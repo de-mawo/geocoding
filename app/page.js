aa66064bcd1dc55e3468eeff5b7bfef3b7d54f49
groovebass <groovebassist1@gmail.com>
2023-06-14 00:00:04 +0200

    Added gray background

--- app/page.js
+++ app/page.js
@@ -1,11 +1,16 @@
-
-import Header from './components/Header'
+import Header from "./components/Header";
+import Link from "next/link";
 export default function Home() {
   return (
     <main >
       
       <Header />
-      <div>Home Page</div>
+      <div className="flex flex-col items-center justify-center   py-8 px-6 space-y-5 mb-24">
+        <h1 className="text-2xl font-bold">Home Page </h1>
+        <Link href="/cart" className="p-2 bg-white shadow-lg rounded-md font-semibold text-gray-500">
+          Go to Cart
+        </Link>
+      </div>
     </main>
   );
 }
